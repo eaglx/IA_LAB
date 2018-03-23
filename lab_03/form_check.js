28c04338925c72bfd0a629da56560cfc092f4e14
eaglx <tango_delta@outlook.com>
2018-03-23 07:23:26 +0100

    Task 9

--- lab_03/form_check.js
+++ lab_03/form_check.js
@@ -1,8 +1,27 @@
 function isEmpty(arg){
-    if(arg.length == 0) {
-        return true;
-    }
-    else {
+    var str = arg.value;
+    if(str.length)
+        return false
+    else
+        return true
+}
+
+function isWhiteSpace(str) {
+     var ws = "\t\n\r ";
+     for (var i = 0; i < str.length; i++) {
+         var c = str.charAt(i);
+         if (ws.indexOf(c) == -1) {
+             return false;
+             }
+         }
+     return true;
+}
+
+function validate(form){
+    if(isEmpty(form.elements["f_imie"])){
+        alert("Podaj imie");
         return false;
     }
+
+    return true;
 }
