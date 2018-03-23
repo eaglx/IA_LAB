ead99c87243ca0bb52087a2bf6dfeb15135353e9
eaglx <tango_delta@outlook.com>
2018-03-23 07:58:34 +0100

    Task 21a

--- lab_03/form_check.js
+++ lab_03/form_check.js
@@ -72,12 +72,12 @@ function checkStringAndFocus(obj, msg) {
 }
 
 function validate(form){
-    if((!checkStringAndFocus(form.elements["f_imie"], "Podaj imie"))){
-        //|| (!checkStringAndFocus(form.elements["f_nazwisko"], "Podaj nazwisko"))
-        //|| (!checkStringAndFocus(form.elements["f_kod"], "Podaj kod"))
-        //|| (!checkStringAndFocus(form.elements["f_ulica"], "Podaj ulice"))
-        //|| (!checkStringAndFocus(form.elements["f_miasto"], "Podaj miasto"))
-        //|| (!checkEmail(form.elements["f_email"]))){
+    if((!checkStringAndFocus(form.elements["f_imie"], "Podaj imie"))
+        || (!checkStringAndFocus(form.elements["f_nazwisko"], "Podaj nazwisko"))
+        || (!checkStringAndFocus(form.elements["f_kod"], "Podaj kod"))
+        || (!checkStringAndFocus(form.elements["f_ulica"], "Podaj ulice"))
+        || (!checkStringAndFocus(form.elements["f_miasto"], "Podaj miasto"))
+        || (!checkEmail(form.elements["f_email"]))){
         return false;
     }
 
