8a6269ca3d40263cc2aa75f01d87c25c366d1918
eaglx <tango_delta@outlook.com>
2018-03-23 07:43:43 +0100

    Task 14

--- lab_03/form_check.js
+++ lab_03/form_check.js
@@ -1,6 +1,5 @@
 function isEmpty(arg){
-    var str = arg.value;
-    if(str.length)
+    if(arg.length)
         return false
     else
         return true
@@ -17,9 +16,51 @@ function isWhiteSpace(str) {
      return true;
 }
 
+function checkEmail(obj) {
+    var str = obj.value;
+
+    if (isWhiteSpace(str)) {
+        alert("Podaj właściwy e-mail");
+        return false;
+    }
+    else {
+        var at = str.indexOf("@");
+        if (at < 1) {
+            alert("Nieprawidłowy e-mail");
+            return false;
+        }
+        else {
+            var l = -1;
+            for (var i = 0; i < str.length; i++) {
+                var c = str.charAt(i);
+                if (c == ".") {
+                    l = i;
+                }
+            }
+            if ((l < (at + 2)) || (l == str.length - 1)) {
+                alert("Nieprawidłowy e-mail");
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+function checkString(toCheck, message) {
+    if(isEmpty(toCheck.value) || isWhiteSpace(toCheck.value)){
+        alert(message)
+        return false
+    }
+    return true
+}
+
 function validate(form){
-    if(isEmpty(form.elements["f_imie"])){
-        alert("Podaj imie");
+    if((!checkString(form.elements["f_imie"], "Podaj imie"))
+        || (!checkString(form.elements["f_nazwisko"], "Podaj nazwisko"))
+        || (!checkString(form.elements["f_kod"], "Podaj kod"))
+        || (!checkString(form.elements["f_ulica"], "Podaj ulice"))
+        || (!checkString(form.elements["f_miasto"], "Podaj miasto"))
+        || (!checkEmail(form.elements["f_email"]))){
         return false;
     }
 
